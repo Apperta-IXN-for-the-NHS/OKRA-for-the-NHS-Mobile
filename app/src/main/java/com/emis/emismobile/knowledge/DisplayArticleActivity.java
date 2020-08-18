d97241cf1b1d440dbfc2b9eb63ceaa5b261b3765
Andrea <andrea.naudi@gmail.com>
2020-08-18 16:49:51 +0200

    display article screen styling

--- app/src/main/java/com/emis/emismobile/knowledge/DisplayArticleActivity.java
+++ app/src/main/java/com/emis/emismobile/knowledge/DisplayArticleActivity.java
@@ -4,10 +4,19 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.emis.emismobile.R;
 
@@ -15,6 +24,10 @@ public class DisplayArticleActivity extends AppCompatActivity {
     private TextView bodyTextView;
     private TextView authorTextView;
     private TextView dateTextView;
+    private TextView titleTextView;
+    private LinearLayout buttonsLayout;
+    private ScrollView scrollView;
+    private Button likeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +36,13 @@ public class DisplayArticleActivity extends AppCompatActivity {
         bodyTextView = this.findViewById(R.id.article_body);
         authorTextView = this.findViewById(R.id.article_author);
         dateTextView = this.findViewById(R.id.article_date);
+        titleTextView = this.findViewById(R.id.article_title);
+        likeButton = this.findViewById(R.id.likeButton);
+        buttonsLayout = this.findViewById(R.id.buttons_layout);
+        scrollView = this.findViewById(R.id.article_scrollview);
 
         KnowledgeViewModel viewModel = new ViewModelProvider(this).get(KnowledgeViewModel.class);
-
+        setUpScroll();
         Intent intent = getIntent();
         String articleId = intent.getStringExtra("article_id");
 
@@ -42,7 +59,8 @@ public class DisplayArticleActivity extends AppCompatActivity {
 
             authorTextView.setText(article.getAuthor());
             dateTextView.setText(article.getDate());
-            setTitle(article.getTitle());
+            titleTextView.setText(article.getTitle());
+            setTitle("Article");
         } else {
             bodyTextView.setText("This article is empty.");
         }
@@ -52,5 +70,25 @@ public class DisplayArticleActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    private void setUpScroll() {
+        scrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+            float y = 0;
+
+            @Override
+            public void onScrollChanged() {
+                if (scrollView.getScrollY() > y) {
+                    //scroll down
+                    buttonsLayout.setVisibility(View.GONE);
+                    System.out.println("down");
+                } else {
+                    //scroll up
+                    buttonsLayout.setVisibility(View.VISIBLE);
+                    System.out.println("up");
+                }
+                y = scrollView.getScrollY();
+            }
+        });
     }
 }
